ec8e344907904dfa815c4b874a09078dd98a5c76
Omkar Gutal <gutalomkar01@gmail.com>
2026-03-13 12:21:37 +0530

    Integrated multiple LLM Models like Ollama, llama, LM Studio and Jan Functionality for local machine

--- config/search.py
+++ config/search.py
@@ -8,16 +8,23 @@ LinkedIn:   https://www.linkedin.com/in/omkar-gutal-a25935249/
 
 # These Sentences are Searched in LinkedIn
 # Enter your search terms inside '[ ]' with quotes ' "searching title" ' for each search followed by comma ', ' Eg: ["Software Engineer", "Software Developer", "Selenium Developer"]
-search_terms = ["Data Analyst", "Analyst", "Power BI Developer", "Data Engineer", "Data Consultant"]
+search_terms = [
+    "AI Engineer", "Artificial Intelligence Engineer", "Generative AI Engineer", "GenAI Engineer", "LLM Engineer", 
+    "AI Developer", "AI Research Engineer", "AI Solutions Architect",
+    "Machine Learning Engineer", "ML Engineer", "MLOps Engineer", "Machine Learning Infrastructure Engineer", 
+    "Deep Learning Engineer", "Computer Vision Engineer", "NLP Engineer", "ML Researcher",
+    "Data Scientist", "Applied Scientist", "Data Science Engineer", "Data Science Specialist", 
+    "Lead Data Scientist", "Senior Data Scientist"
+]
 
 # Search location, this will be filled in "City, state, or zip code" search box. If left empty as "", tool will not fill it.
-search_location = ""               # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
+search_location = "India"               # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
 
 # After how many number of applications in current search should the bot switch to next search? 
 switch_number = 25                 # Only numbers greater than 0... Don't put in quotes
 
 # Do you want to randomize the search order for search_terms?
-randomize_search_order = False     # True of False, Note: True or False are case-sensitive
+randomize_search_order = True     # True of False, Note: True or False are case-sensitive
 
 
 # >>>>>>>>>>> Job Search Filters <<<<<<<<<<<
@@ -37,13 +44,13 @@ This is below format: QUESTION = VALID_ANSWER
 
 '''
 
-sort_by = ""                       # "Most recent", "Most relevant" or ("" to not select) 
-date_posted = "Any time"         # "Any time", "Past month", "Past week", "Past 24 hours" or ("" to not select)
+sort_by = "Most recent"            # "Most recent", "Most relevant" or ("" to not select) 
+date_posted = "Past week"          # "Any time", "Past month", "Past week", "Past 24 hours" or ("" to not select)
 salary = ""                        # "$40,000+", "$60,000+", "$80,000+", "$100,000+", "$120,000+", "$140,000+", "$160,000+", "$180,000+", "$200,000+"
 
 easy_apply_only = True             # True or False, Note: True or False are case-sensitive
 
-experience_level = ["Internship", "Entry level", "Associate"]              # (multiple select) "Internship", "Entry level", "Associate", "Mid-Senior level", "Director", "Executive"
+experience_level = ["Entry level", "Associate"]              # (multiple select) "Internship", "Entry level", "Associate", "Mid-Senior level", "Director", "Executive"
 job_type = []                      # (multiple select) "Full-time", "Part-time", "Contract", "Temporary", "Volunteer", "Internship", "Other"
 on_site = []                       # (multiple select) "On-site", "Remote", "Hybrid"
 
